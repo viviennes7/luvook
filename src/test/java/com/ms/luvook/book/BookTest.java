1c760f6ab0947b5d8afc9bdab8463227b3ad6e95
ylland_IT2 <ylland_IT2@ylland_IT2-PC>
2017-05-23 10:58:51 +0900

    modify test code

--- src/test/java/com/ms/luvook/book/BookTest.java
+++ src/test/java/com/ms/luvook/book/BookTest.java
@@ -1,19 +1,18 @@
 package com.ms.luvook.book;
 
-import com.google.gson.Gson;
-import com.ms.luvook.book.type.ItemIdType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 /**
  * Created by vivie on 2017-05-13.
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BookTest {
     private WebClient webClient = WebClient.create("http://localhost:8080");
 
