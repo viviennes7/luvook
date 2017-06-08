446d3585ed1f97c7c64ce19645d1bae4069ea73d
김민수 <viviennes7@naver.com>
2017-06-09 00:26:46 +0900

    modify book

--- src/main/java/com/ms/luvook/book/controller/BookController.java
+++ src/main/java/com/ms/luvook/book/controller/BookController.java
@@ -1,22 +1,15 @@
 package com.ms.luvook.book.controller;
 
-import java.time.Duration;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ms.luvook.book.domain.Book;
 import com.ms.luvook.book.domain.BookSearch;
 import com.ms.luvook.book.service.BookService;
 import com.ms.luvook.book.type.ItemIdType;
 import com.ms.luvook.book.type.QueryType;
-import com.ms.luvook.member.domain.Member;
-
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 /**
@@ -29,19 +22,6 @@ public class BookController{
 
     @Autowired
     private BookService bookService;
-
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE , value="/test/{query}")
-    public Flux<Integer> test(@PathVariable String query){
-        new Member("1",2,"a");
-//        Flux<Book> bookFlux = Flux.fromStream(Stream.generate(() -> new Member("1",2,"a")));
-
-        Flux<Long> durationFlux = Flux.interval(Duration.ofSeconds(1));
-
-
-        return Flux
-                .range(1,100)
-                .map(s -> s * 10);
-    }
 
     @GetMapping("/books/{itemId}")
     public Mono<Book> findOne(@PathVariable String itemId, String itemIdType) throws Exception{
