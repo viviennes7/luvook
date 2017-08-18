44fb3f5c99a525cf24f28512c999b8f1716bc754
yl_Minsoo <viviennes7@naver.com>
2017-08-18 18:01:52 +0900

    annotate LogAspect

--- src/main/java/com/ms/luvook/common/aspect/LogAspect.java
+++ src/main/java/com/ms/luvook/common/aspect/LogAspect.java
@@ -1,11 +1,6 @@
 package com.ms.luvook.common.aspect;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,46 +11,47 @@ import lombok.extern.slf4j.Slf4j;
 public class LogAspect {
 	final static String ELAPSED_UNIT = "(ms)";
 	
-	@Pointcut("execution(* com.ms.luvook..controller.*Controller.*(..))")
-	public void controllers() {}
+//	@Pointcut("execution(* com.ms.luvook..controller.*Controller.*(..))")
+//	public void controllers() {}
+//	
+//	@Pointcut("@annotation(com.ms.luvook.common.annotation.BasicLog)")
+//	public void basicLog(){}
+//	
+//	@Around("controllers() || basicLog()")
+//	public Object basic(ProceedingJoinPoint joinPoint) throws Throwable{
+//		
+//		long start = System.currentTimeMillis();
+//		log.info("########## Start ::::: {} ##########", joinPoint.getSignature().toShortString() );
+//		
+//		logParameters(joinPoint);
+//		
+//		Object output = joinPoint.proceed();
+//		
+//		long end = System.currentTimeMillis();
+//		log.info("########## End ::::: {} / {}##########", joinPoint.getSignature().toShortString(), (end-start) + ELAPSED_UNIT );
+//		
+//		log.info("result ::::: {}", output);
+//		
+//		return output;
+//	}
+//	
+//	public void logParameters(JoinPoint joinPoint){
+//		String result = null;
+//		StringBuilder sb = new StringBuilder("param ::::: ");
+//		
+//		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+//		String[] parameterNames = signature.getParameterNames();
+//		Object[] parameterValues = joinPoint.getArgs();
+//
+//		for (int i = 0; i < parameterValues.length; i++) {
+//			sb.append(parameterNames[i]);
+//			sb.append(" : ");
+//			sb.append(parameterValues[i]);
+//			sb.append(" , ");
+//		}
+//		result = sb.toString();
+//		
+//		log.info(result);
+//	}
 	
-	@Pointcut("@annotation(com.ms.luvook.common.annotation.BasicLog)")
-	public void basicLog(){}
-	
-	@Around("controllers() || basicLog()")
-	public Object basic(ProceedingJoinPoint joinPoint) throws Throwable{
-		
-		long start = System.currentTimeMillis();
-		log.info("########## Start ::::: {} ##########", joinPoint.getSignature().toShortString() );
-		
-		logParameters(joinPoint);
-		
-		Object output = joinPoint.proceed();
-		
-		long end = System.currentTimeMillis();
-		log.info("########## End ::::: {} / {}##########", joinPoint.getSignature().toShortString(), (end-start) + ELAPSED_UNIT );
-		
-		logResult(output);
-		
-		return output;
-	}
-	
-	public void logParameters(JoinPoint joinPoint){
-		String result = "param ::::: "; 
-		
-		final MethodSignature signature = (MethodSignature)joinPoint.getSignature();
-		String[] parameterNames = signature.getParameterNames();
-		Object[] parameterValues = joinPoint.getArgs();
-
-		for (int i = 0; i < parameterValues.length; i++) {
-			result += parameterNames[i] + " : ";
-			result += parameterValues[i] + " , ";
-		}
-		
-		log.info(result);
-	}
-	
-	public void logResult(Object output){
-		log.info("result ::::: {}", output);
-	}
 }
