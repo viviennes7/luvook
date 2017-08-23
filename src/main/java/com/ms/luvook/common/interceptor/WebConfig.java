089add53d2d2a9ded348bc2f61ff271cb4c5ea54
yl_Minsoo <viviennes7@naver.com>
2017-08-24 03:18:49 +0900

    modify excludePath

--- src/main/java/com/ms/luvook/common/interceptor/WebConfig.java
+++ src/main/java/com/ms/luvook/common/interceptor/WebConfig.java
@@ -9,9 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 	private static final String[] EXCLUDE_PATHS = {
-			"/member/signup",
-			"/member/signin",
-			"/member/validate",
+			"/member/**",
 			"/error/**"
 			};
 
