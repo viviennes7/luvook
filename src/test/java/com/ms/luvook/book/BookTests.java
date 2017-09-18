bee9215ee9862a6db9a5e4dc10a2a7f959f775b6
yl_Minsoo <viviennes7@naver.com>
2017-09-18 15:18:50 +0900

    add @After

--- src/test/java/com/ms/luvook/book/BookTests.java
+++ src/test/java/com/ms/luvook/book/BookTests.java
@@ -1,5 +1,6 @@
 package com.ms.luvook.book;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,9 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by vivie on 2017-05-13.
+ * 
+ * 임시로 작성한 테스트코드입니다.
  */
-
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -25,19 +27,26 @@ public class BookTests {
 	@Autowired
 	private BookService bookService;  
 	
+	@After
+	public void daemonThread(){
+		try {
+			Thread.sleep(3 * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
     @Test
-    public void find() throws Exception {
+    public void find(){
     	bookService.find("동화", QueryType.TITLE, 0, 50).subscribe(result -> {
     		log.info("find() ::: {}", result);
     	});
-    	Thread.sleep(5000);
     }
     
     @Test
-    public void findOne() throws Exception{
+    public void findOne(){
     	bookService.findOne("9788949113623", ItemIdType.ISBN13).subscribe(result -> {
     		log.info("findOne() ::: {}", result);
     	}) ;
-    	Thread.sleep(5000);
     }
 }
