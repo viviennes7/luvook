f245c25362dbfa6a0c0bed37664d433c7c95a499
yl_Minsoo <viviennes7@naver.com>
2017-09-22 14:24:12 +0900

    refactoring all and add saveAndFind(), findAll(), update()

--- src/test/java/com/ms/luvook/board/BoardCommentTests.java
+++ src/test/java/com/ms/luvook/board/BoardCommentTests.java
@@ -4,7 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
-import java.util.Date;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -15,12 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.ms.luvook.board.domain.Board;
 import com.ms.luvook.board.domain.BoardComment;
+import com.ms.luvook.board.domain.BookBoard;
 import com.ms.luvook.board.service.BoardService;
 import com.ms.luvook.common.domain.IsUse;
 import com.ms.luvook.member.domain.MemberMaster;
-import com.ms.luvook.member.domain.MemberType;
-import com.ms.luvook.member.repository.MemberRepository;
+import com.ms.luvook.member.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,28 +35,57 @@ public class BoardCommentTests {
 	private BoardService boardService;
 
 	@Autowired
-	private MemberRepository memberRepository;
+	private MemberService memberService;
 
 	private MemberMaster memberMaster;
 	private int memberId;
+	private int boardId;
 	private BoardComment boardComment;
-
 
 	@Before
 	public void setup(){
-		memberMaster = new MemberMaster("%test_nickname",  "%test1@naver.com", "123123", "img", MemberType.USER, new Date(), new Date());
-		memberMaster = memberRepository.save(memberMaster);
+		memberMaster = new MemberMaster("%test_nickname",  "%test1@naver.com", "123123", null, null, null, null);
+		memberMaster = memberService.signup(memberMaster);
 		memberId = memberMaster.getMemberId();
-		boardComment = new BoardComment(0, 1, null,1, "댓글테스트", null, null, null);
+		
+		Board bookBoard = new BookBoard(memberId,"즐거운책", 5, "책 제목", 12345, "img_url", "K12345678", "1234567891263");
+		Board savedBoard = boardService.save(bookBoard, memberId);
+		boardId = savedBoard.getBoardId();
+		
+		boardComment = new BoardComment(0, memberId, null, boardId, "댓글테스트", null, null, null);
 	}
 	
 	@Test
-	public void saveAndFind(){
+	public void save(){
 		//When
 		BoardComment savedComment = boardService.saveComment(boardComment, memberId);
 		
 		//Then
 		assertNotNull(savedComment);
+	}
+	
+	@Test
+	public void setupForSave(){
+		//When
+		BoardComment savedComment = boardService.saveComment(boardComment, memberId);
+		
+		//Then
+		assertThat(savedComment.getMemberId(), is(memberId));
+		assertThat(savedComment.getIsUse(), is(IsUse.Y));
+		assertNotNull(savedComment.getContents());
+	}
+	
+	@Test
+	public void findAll(){
+		//Given
+		BoardComment savedComment = boardService.saveComment(boardComment, memberId);
+		
+		//When
+		List<BoardComment> comments = boardService.findAllComment(boardId);
+		BoardComment commentInList = comments.get(0);
+		
+		//Then
+		assertThat(savedComment.getContents(), is(commentInList.getContents()));
 	}
 	
 	@Test
@@ -69,5 +99,21 @@ public class BoardCommentTests {
 		
 		//Then
 		assertThat(savedComment.getIsUse(), is(IsUse.N));
+	}
+	
+	@Test
+	public void update(){
+		//Given
+		BoardComment savedComment = boardService.saveComment(boardComment, memberId);
+		String willUpdateContents = "댓글 변경 테스트";
+		
+		//When
+		savedComment.setContents(willUpdateContents);
+		boardService.updateComment(savedComment);
+		
+		//Then
+		List<BoardComment> comments = boardService.findAllComment(boardId);
+		BoardComment updatedComment = comments.get(0);
+		assertThat(updatedComment.getContents(), is(willUpdateContents));
 	}
 }
